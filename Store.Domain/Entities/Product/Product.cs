babdef3f9159d19969b0eea8524fd86441908097
Alejandro Gil <gil.alejandro@c-ven.com>
2022-03-01 19:30:49 -0400

    Add Entities, DB Context, User Class

---
+++ Store.Domain/Entities/Product/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.Product
+{
+    public class Product
+    {
+        [Required]
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(85)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+}
---
+++ Store.Domain/Entities/User/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Store.Domain.Entities.User
+{
+    public class User : IdentityUser<Guid>
+    {
+        public Guid CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Guid ModifiedBy { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
