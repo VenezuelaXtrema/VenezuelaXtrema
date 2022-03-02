a905c36eed19506ccac7554b7e5d4476f3800d1b
Alejandro Gil <gil.alejandro@c-ven.com>
2022-03-01 21:02:25 -0400

    Adding Controllers

---
+++ Store.Api/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/{version:apiVersion}/[controller]")]
+    public class BaseController : ControllerBase
+    {
+
+    }
+}
